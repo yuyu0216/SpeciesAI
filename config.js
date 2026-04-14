99ab1073b2018360db52a72f261424a420d4dedb
yuyu0216 <summerkitty50@gmail.com>
2026-04-15 03:18:43 +0800

    chat update

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  */
 
 // ▶ 將你的 Apps Script Web App URL 貼在這裡
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbx25vkIh5aiGpIHXhwtvJaEVncs7MxDWto5as3DNNz9HcJ78l6aAjH9vX7iKMO8VHa8/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwdfiFP6JLbdnZpXm1e65deuc6QBSXfDmuOPqrl5U4ppirOKk2u7k48vJARlHGYHBJw/exec';
 
 // ▶ 給 admin 頁面的額外匯出密碼 (可留空)
 const EXPORT_SECRET = '';
